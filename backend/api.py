2a5101cb7e04923050028b7c40079a408d97f79d
vy1216 <vy6490134@gmail.com>
2026-03-01 18:48:03 +0530

    commit

--- backend/api.py
+++ backend/api.py
@@ -28,7 +28,8 @@ def get_connection():
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", 5432)
     )
 
 # --------------------------------------------------
